198e2bdbf26d1f829351cc714e9c7c0fb6ebd5d3
MacBookPro <dalaaal@gmail.com>
2015-04-20 05:09:09 +0300

    added

--- app/src/main/java/com/example/macbookpro/davitaproject/sign_in.java
+++ app/src/main/java/com/example/macbookpro/davitaproject/sign_in.java
@@ -1,37 +1,165 @@
 package com.example.macbookpro.davitaproject;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.Editable;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Toast;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
 public class sign_in extends Activity {
     public final static String EXTRA_MESSAGE = "com.example.macbookpro.davitaproject.MESSAGE";
-
+    Button bb1;
+    EditText ee1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-
-
-
-
-        Button bb1 = (Button) findViewById(R.id.b1);
-        TextView t1=(TextView) findViewById(R.id.ed1);
-        TextView t2=(TextView) findViewById(R.id.textView2);
-
-
-
-
+       bb1 = (Button) findViewById(R.id.b1);
     }
+
+
+
+
+
+
+        class tesfir extends AsyncTask<String, String, Void> {
+
+        private ProgressDialog prdi=new ProgressDialog(sign_in.this);
+            InputStream is=null;
+            String result="";
+
+            @Override
+            protected  void  onPreExecute(){
+                prdi.setMessage("please wait");
+                prdi.show();
+                prdi.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface arg0) {
+                        tesfir.this.cancel(true);
+                    }
+                });
+
+            }
+
+            @Override
+            protected Void doInBackground(String... params) {
+                String url="http://davitacare.byethost24.com/demo.php";
+                HttpClient hc=new DefaultHttpClient();
+                HttpPost hp= new HttpPost(url);
+
+                ArrayList<NameValuePair> param =new ArrayList<NameValuePair>();
+
+                try{
+                    hp.setEntity(new UrlEncodedFormEntity(param));
+                    HttpResponse hr=hc.execute(hp);
+                    HttpEntity he=hr.getEntity();
+                    is=he.getContent();
+
+
+                } catch (Exception e) {
+                    Log.e("Log_tag", "Error  " + e.toString());
+                    Toast.makeText(sign_in.this, "please try again", Toast.LENGTH_LONG).show();
+                }
+                   try{
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                    StringBuilder sb = new StringBuilder();
+                    String line = "";
+                    while((line=br.readLine())!=null)
+                    {
+                        sb.append(line+"\n");
+                    }
+                       is.close();
+                       result=sb.toString();
+
+                   }
+                   catch (Exception e) {
+                       // TODO: handle exception
+                       Log.e("log_tag", "Error converting result "+e.toString());
+                   }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void v) {
+
+
+                try {
+                    JSONArray Jarray = new JSONArray(result);
+                    for(int i=0;i<Jarray.length();i++)
+                    {
+
+                        JSONObject Jasonobject = null;
+                        ee1=(EditText) findViewById(R.id.pass);
+                        Jasonobject = Jarray.getJSONObject(i);
+
+
+                        String id = Jasonobject.getString("pid");
+
+                        if(ee1.getText().toString().equals(id)) {
+
+                            Intent intent = new Intent(sign_in.this, MainActivity2Activity.class);
+
+                            intent.putExtra(EXTRA_MESSAGE,id);
+                                startActivity(intent);
+                            break;
+
+                        }
+                    }
+                    this.prdi.dismiss();
+                }
+                catch (Exception e) {
+                    // TODO: handle exception
+                    Log.e("log_tag", "Error parsing data "+e.toString());
+                }
+            }
+        }
+    public void maybthlast(View view) {
+        switch (view.getId()) {
+            case R.id.b1:
+                new tesfir().execute();
+                break;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     @Override
@@ -56,18 +184,6 @@ public class sign_in extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void maybthlast(View view) {
 
-
-
-        Intent intent = new Intent(this, MainActivity2Activity.class);
-
-        EditText ee1=(EditText) findViewById(R.id.pass);
-        String st=ee1.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, st);
-        if(st.equals("1075000579"))
-        startActivity(intent);
-
-
-    }
 }
+
