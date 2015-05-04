c5f17a3644f482c74f5520a52a2ae96788aa202a
MacBookPro <dalaaal@gmail.com>
2015-05-04 23:14:23 +0300

    update- so many things

--- app/src/main/java/com/example/macbookpro/davitaproject/AlarmReceiver.java
+++ app/src/main/java/com/example/macbookpro/davitaproject/AlarmReceiver.java
@@ -9,7 +9,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.PowerManager;
